7d05bca02b35183474fd4e42ccb3ddec63b73614
ederbueno <bueno.ederson@gmail.com>
2021-02-18 00:16:57 -0300

    criando formulário

--- app/src/main/java/br/com/edma/fitnessmanager/MainActivity.java
+++ app/src/main/java/br/com/edma/fitnessmanager/MainActivity.java
@@ -2,13 +2,26 @@ package br.com.edma.fitnessmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View btn_imc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_imc = findViewById(R.id.btn_imc);
+
+        btn_imc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ImcActivity.class));
+            }
+        });
     }
 }
